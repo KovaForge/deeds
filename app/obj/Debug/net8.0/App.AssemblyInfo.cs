3f94adb077c5d960120a0257b739af74ac61457a
Michael D <McoreD@users.noreply.github.com>
2025-12-22 12:31:02 +0000

    Add Settings navigation button and change currency from £ to $

--- app/obj/Debug/net8.0/App.AssemblyInfo.cs
+++ app/obj/Debug/net8.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d21ea1e8992cb168ea14cc4b11cf8be1500204a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7898cff1db9dee99f97d503906bd491b238b06e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
